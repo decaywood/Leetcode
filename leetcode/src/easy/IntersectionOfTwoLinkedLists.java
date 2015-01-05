c493ce4dfa7dcb515fa0dce0ad26afa877e1ac3f
decaywood <decaywood@decaywood-PC>
2015-01-05 13:18:45 +0800

    new commit

---
+++ leetcode/src/easy/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,88 @@
+package easy;
+
+/**
+ * @author decaywood
+ * 
+ * 2015年1月4日
+ * 
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ * For example, the following two linked lists:
+ * 
+ * A:          a1 → a2
+ *                    ↘
+ *                      c1 → c2 → c3
+ *                    ↗            
+ * B:     b1 → b2 → b3
+ * begin to intersect at node c1.
+ * 
+ * 
+ * Notes:
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function returns.
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ * 
+ */
+public class IntersectionOfTwoLinkedLists {
+    
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode("a1");
+//        node1.append("a2").append("c1").append("c2").append("c3").append("c4").append("c5");
+        ListNode node2 = new ListNode("b1");
+        node2.append("b3").append("b2");
+//        node2.next.next.next = node1.next.next;
+        getIntersectionNode(node1, new ListNode("ss"));
+    }
+    
+    private static class ListNode{
+        String val;
+        ListNode next;
+        public ListNode(String x) {
+            val = x;
+            next = null;
+        }
+        
+        private ListNode append(String x) {
+            next = new ListNode(x);
+            return next;
+        }
+    }
+    
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        if(headA == null ||headB == null)return null;  
+        ListNode h1 = headA;  
+        ListNode h2 = headB;  
+        int count1 = 1, count2 = 1;  
+        while(h1.next != null){  
+            count1++;  
+            h1 = h1.next;  
+        }  
+        while(h2.next != null){  
+            count2++;  
+            h2 = h2.next;  
+        }  
+        if(h1 != h2) return null;  
+        else{  
+            int count = Math.abs(count1 - count2);  
+            if(count2 > count1){  
+                h1 = headB;  
+                h2 = headA;   
+            }  
+            else{  
+                h1 = headA;  
+                h2 = headB;  
+            }  
+            while((count--) > 0){  
+                h1 = h1.next;  
+            }  
+            while(h1 != null&&h2 != null && h1 != h2){  
+                h1 = h1.next;  
+                h2 = h2.next;  
+            }  
+            return h1;  
+        }  
+    }  
+    
+     
+}
