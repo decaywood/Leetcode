3cc42eafdf6e1762df7e3aceab82c5e0136da0ff
decaywood <decaywood@qq.com>
2015-03-29 11:20:25 +0800

    number of 1 bits

--- leetcode/src/easy/BinaryTreeLevelOrderTraversal.java
+++ leetcode/src/easy/BinaryTreeLevelOrderTraversal.java
@@ -46,9 +46,8 @@ public class BinaryTreeLevelOrderTraversal {
 //            level = Math.max(level, offerInqueue(root.right));
 //        return level+1;
 //    }
-    
-    
-    
+
+
     private Queue<TreeNode> queue = new LinkedList<TreeNode>();
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
@@ -61,7 +60,7 @@ public class BinaryTreeLevelOrderTraversal {
         currentLevel = 1;
         
         while (!queue.isEmpty()) {
-            
+
             TreeNode node = queue.poll();
             if(currentLevel == 0) {
                 list.add(childList);
@@ -87,18 +86,5 @@ public class BinaryTreeLevelOrderTraversal {
         return list;
     }
     
-    public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(2);
-        TreeNode node4 = new TreeNode(3);
-        TreeNode node5 = new TreeNode(3);
-        TreeNode node6 = new TreeNode(4);
-        node1.left = node2;
-//        node1.right = node3;
-//        node2.left = node4;
-//        node2.right = node5;
-//        node4.left = node6;
-        new BinaryTreeLevelOrderTraversal().levelOrder(node1);
-    }
+
 }
