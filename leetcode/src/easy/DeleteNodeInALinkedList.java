bd7545c6ea65f366e3598130120ceb8f88fbb442
decaywood <decaywood@qq.com>
2015-07-19 16:34:42 +0800

    update algorithm

---
+++ leetcode/src/easy/DeleteNodeInALinkedList.java
@@ -0,0 +1,31 @@
+package easy;
+
+/**
+ * @author: decaywood
+ * @date: 2015/7/19 11:45
+ */
+public class DeleteNodeInALinkedList {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+
+}
