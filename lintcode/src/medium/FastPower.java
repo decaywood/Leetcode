19e2f9c43ed55342364a5891a025bc7c87c6e067
decaywood <decaywood@qq.com>
2015-08-31 17:22:54 +0800

    1 problem remain unsolved

---
+++ lintcode/src/medium/FastPower.java
@@ -0,0 +1,25 @@
+package medium;
+
+/**
+ * @author: decaywood
+ * @date: 2015/8/31 16:31
+ *
+ * Calculate the a^n % b where a, b and n are all 32bit integers.
+ *
+ * Example
+ * For 2^31 % 3 = 2
+ *
+ * For 1001000 % 1000 = 0
+ *
+ * Challenge
+ * O(logn)
+ *
+ */
+public class FastPower {
+
+    public static int fastPower(int a, int b, int n) {
+        return 0;
+    }
+
+
+}
